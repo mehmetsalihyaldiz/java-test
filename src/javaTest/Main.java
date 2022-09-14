085541ae4731ac7b48f31fe314a0caa86b6934c1
Mehmet Salih YALDIZ <83459326+mehmetsalihyaldiz@users.noreply.github.com>
2022-09-15 02:28:05 +0300

    First.

---
+++ src/javaTest/Main.java
@@ -0,0 +1,17 @@
+package javaTest;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Test JAVA");
+		
+		String adi = "Mehmet Salih";
+		String soyadi = "YALDIZ";
+		
+		System.out.println(adi + " " + soyadi);
+		
+	}
+
+}
